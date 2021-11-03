5081a3037e167936d37c0f91d45ced1f20de9162
clearsky77 <clearsky3264@gmail.com>
2021-11-03 16:00:28 +0900

    임시방편 데이터 입력(하드 코딩)

--- app/src/main/java/com/clearsky77/listview_review/MainActivity.kt
+++ app/src/main/java/com/clearsky77/listview_review/MainActivity.kt
@@ -2,10 +2,20 @@ package com.clearsky77.listview_review
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clearsky77.listview_review.datas.PetData
 
 class MainActivity : AppCompatActivity() {
+    val mPetList = ArrayList<PetData>() // 담을 그릇 만들기
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mPetList.add(PetData("냥냐루", 2015, "식빵 굽기"))
+        mPetList.add(PetData("냐냐루", 2020, "솜방망이 펀치"))
+        mPetList.add(PetData("냥냐냥", 2019, "물건 떨어트리기"))
+        mPetList.add(PetData("리터봇", 2018, "뒷정리"))
+
     }
 }
